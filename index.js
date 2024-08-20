645180b687e31218acfd81a8d7f5dd649219b4f7
Lucas Silveira Gevaerd <lucas.gevaerd@komlog.com.br>
2024-08-20 14:40:15 -0300

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors = require('cors');  // Importa o middleware CORS
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Habilita CORS para todas as origens
+let lastLeadData = null; // Variável para armazenar os últimos dados recebidos
+
+// Middleware para CORS
 app.use(cors());
 
 // Middleware para parsear o corpo das requisições em JSON
@@ -11,13 +13,25 @@ app.use(express.json());
 
 // Rota para receber os dados do webhook
 app.post('/webhook', (req, res) => {
-  const leadData = req.body;
-  
+  lastLeadData = req.body;
+
   // Exibe os dados no log
-  console.log('Dados recebidos do RD Station:', leadData);
+  console.log('Dados recebidos do RD Station:', lastLeadData);
 
   // Envia uma resposta de sucesso
   res.status(200).send('Dados recebidos com sucesso!');
+});
+
+// Rota para exibir os dados na tela
+app.get('/', (req, res) => {
+  if (lastLeadData) {
+    res.send(`
+      <h1>Dados Recebidos do RD Station</h1>
+      <pre>${JSON.stringify(lastLeadData, null, 2)}</pre>
+    `);
+  } else {
+    res.send('<h1>Nenhum dado recebido ainda.</h1>');
+  }
 });
 
 // Inicia o servidor
